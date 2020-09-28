af423a2dc66eb71bba2b0904b99b775879685852
Edgar Zuñiga Aguilar <eigan@hotmail.com>
2020-09-27 20:32:35 -0500

    nueva page para el consumo de json(Lista y 3 items)

---
+++ flutter_components/lib/src/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/src/providers/menu_provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes'),
+        ),
+        body: _getLista());
+  }
+
+  Widget _getLista() {
+    menuProvider.cargarData();
+    return ListView(
+      children: _listaItem(),
+    );
+  }
+
+  List<Widget> _listaItem() {
+    return [
+      ListTile(title: Text('Hola Mundo')),
+      ListTile(title: Text('Hola Mundo2')),
+      ListTile(title: Text('Hola Mundo3'))
+    ];
+  }
+}
