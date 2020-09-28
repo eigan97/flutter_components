92d31cd80c3bfa757b501dab4778faddf4e6b442
Edgar Zuñiga Aguilar <eigan@hotmail.com>
2020-09-27 22:06:23 -0500

    Creacion de pages para redireccion y boton custom de retorno

---
+++ flutter_components/lib/src/pages/alert_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AlertPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alertas'),
+      ),
+      //Creacion de boton custom de return
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_location),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
