1398a7a7c1f66568d29a03133b4ee27536fa07a9
Edgar Zuñiga Aguilar <eigan@hotmail.com>
2020-09-27 20:32:09 -0500

    Se modifica para apuntar al hompage creado para el comssumo de json

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
