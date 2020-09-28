cce9fc7c56277901128be4b2ad1eaab7fd364538
Edgar Zuñiga Aguilar <eigan@hotmail.com>
2020-09-27 19:33:55 -0500

    Generacion de listas dinamicas atraves de otra lista de enteros y valores de subtitle leading trailing onTap del ListTile

--- flutter_components/lib/src/pages/home_temp.dart
+++ flutter_components/lib/src/pages/home_temp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  List<int> contactos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,23 +11,53 @@ class HomePageTemp extends StatelessWidget {
       ),
       //List View genera una pestaña con una vista de lista
       body: ListView(
-        children: _generateList(),
+        // children: _generateList(),
+        children: _generateListCorto(),
       ),
     );
   }
 
+  /*
+   * Hay dos formas para crear listTile o items de lista en flutter
+   * forma uno _generateList
+   * forma dos _generateListCorto
+  */
   List<Widget> _generateList() {
-    List<int> contactos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    final List<Widget> listaItems = [];
+    final List<Widget> listaItems = new List<Widget>();
     //divider puede ser ocupado para poner una linea o un divisor entre la lista
-    final Widget listDivided = Divider();
-    for (var item in contactos) {
+    for (int item in contactos) {
       final Widget listItem = ListTile(
-        title: Text('No. $item - List item title'),
+        title: Text('No. $item - List item title.'),
       );
-      listaItems.add(listItem);
-      listaItems.add(listDivided);
+      final Widget listDivided = Divider();
+      // '..' con el doble punto referencio a la misma lista para seguir haciendo el add
+      //es un poco mas limpio el codigo
+      listaItems..add(listItem)..add(listDivided);
     }
     return listaItems;
+  }
+
+  List<Widget> _generateListCorto() {
+    //.map regresa un iterable que no es lo mismo que una lista por lo que
+    //hay que llamar el metodo toList() esto es un metodo mas simple;
+    return contactos.map((item) {
+      final Widget listItem = Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('No. $item - List item title.'),
+            //estos son metodo de subtitle que permite añadir un valor mas pequeño
+            subtitle: Text('$item Cualquier cosa.'),
+            //Leading posiciona un icono al pincipio
+            leading: Icon(Icons.phone),
+            //trailing posisciona un icono al final
+            trailing: Icon(Icons.keyboard_arrow_right),
+            //ontop es la funcion que se ejecutara la dar click a un elemento, añade el efecto de click de material al registro de la lista
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+      return listItem;
+    }).toList();
   }
 }
