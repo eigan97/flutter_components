9989d5f95fb2bea65325ae58ae0263272580b683
Edgar Zuñiga Aguilar <eigan@hotmail.com>
2020-10-04 17:22:23 -0500

    Se añade GestureDetector para cuando se de click en la imagen del circleAvatar

--- flutter_components/lib/src/pages/avatar_page.dart
+++ flutter_components/lib/src/pages/avatar_page.dart
@@ -4,38 +4,46 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Avatar'),
-          //parecido al show dialog que usamos
-          actions: [
-            Container(
-              padding: EdgeInsets.all(5.0),
+      appBar: AppBar(
+        title: Text('Avatar'),
+        //parecido al show dialog que usamos
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            //GestureDetector para detectar cuando se le da click al hijo
+            child: GestureDetector(
+              onTap: () {
+                print('tap');
+              },
               child: CircleAvatar(
                 backgroundImage: NetworkImage(
                     'https://avatars1.githubusercontent.com/u/24250197?s=460&u=c99c28e125ad31490f4971eb1420605c3286e432&v=4'),
                 radius: 25.0,
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(right: 10),
-              child: CircleAvatar(
-                child: Text(
-                  'SL',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text(
+                'SL',
+                style: TextStyle(
+                  color: Colors.white,
                 ),
-                backgroundColor: Colors.purple,
               ),
-            )
-          ],
-        ),
-        body: Center(
-            child: FadeInImage(
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
           image: NetworkImage(
               'https://avatars1.githubusercontent.com/u/24250197?s=460&u=c99c28e125ad31490f4971eb1420605c3286e432&v=4'),
           placeholder: AssetImage('assets/jar-loading.gif'),
           fadeInDuration: Duration(milliseconds: 200),
-        )));
+        ),
+      ),
+    );
   }
 }
