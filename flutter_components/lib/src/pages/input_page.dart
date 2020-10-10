2e682d3977fb7dca9493b52c88687c48e3a258fd
LAPTOP-KJH26PLH\eigan <eigan@hotmail.com>
2020-10-10 15:10:23 -0500

    input text basico, configutacion de inputDecoration

--- flutter_components/lib/src/pages/input_page.dart
+++ flutter_components/lib/src/pages/input_page.dart
@@ -8,8 +8,52 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  String _nombre = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input - Widget'),
+      ),
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          children: <Widget>[
+            _crearInput(),
+            Divider(),
+            _crearPersona(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //
+  Widget _crearInput() {
+    return TextField(
+      onChanged: (value) {
+        setState(() {
+          _nombre = value;
+        });
+      },
+      autofocus: true,
+      textCapitalization: TextCapitalization.words,
+      //decoration
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        counter: Text('Letras ${_nombre.length}'),
+        labelText: 'Nombre de la persona',
+        hintText: 'Jhon Doe',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.person_rounded),
+        icon: Icon(Icons.account_box),
+      ),
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(title: Text('Nombre es: $_nombre'));
   }
 }
--- flutter_components/lib/src/routes/routes.dart
+++ flutter_components/lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:flutter_components/src/pages/animated_container.dart';
 import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/cards_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
+import 'package:flutter_components/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return {
@@ -13,5 +14,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/avatar': (BuildContext context) => AvatarPage(),
     '/card': (BuildContext context) => CardsPage(),
     '/animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    '/input': (BuildContext context) => InputPage(),
   };
 }
