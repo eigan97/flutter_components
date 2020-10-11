d7a5aac9e6a3a9ac3eeba78571b237e98552663c
LAPTOP-KJH26PLH\eigan <eigan@hotmail.com>
2020-10-11 00:12:33 -0500

    Input Date, password, email

--- flutter_components/lib/src/pages/input_page.dart
+++ flutter_components/lib/src/pages/input_page.dart
@@ -9,6 +9,11 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
+  String _email = '';
+  String _fecha = '';
+  String _password = '';
+
+  TextEditingController _inputDateController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +25,12 @@ class _InputPageState extends State<InputPage> {
           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
           children: <Widget>[
             _crearInput(),
+            Divider(),
+            _createInputEmail(),
+            Divider(),
+            _createPassword(),
+            Divider(),
+            _createFecha(context),
             Divider(),
             _crearPersona(),
           ],
@@ -54,6 +65,93 @@ class _InputPageState extends State<InputPage> {
   }
 
   Widget _crearPersona() {
-    return ListTile(title: Text('Nombre es: $_nombre'));
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Este es email: $_email'),
+    );
+  }
+
+  Widget _createInputEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+      autofocus: true,
+      //decoration
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        labelText: 'Email',
+        hintText: 'example@dominio',
+        helperText: 'Correo Electronico',
+        suffixIcon: Icon(Icons.email),
+        icon: Icon(Icons.alternate_email),
+      ),
+    );
+  }
+
+  Widget _createPassword() {
+    return TextField(
+      obscureText: true,
+      onChanged: (value) {
+        setState(() {
+          _password = value;
+        });
+      },
+      autofocus: true,
+      //decoration
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        labelText: 'Password',
+        hintText: 'Password',
+        helperText: '(Mayor a 8, mayuscula, minuscula, numeros, signos)',
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock_rounded),
+      ),
+    );
+  }
+
+  Widget _createFecha(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false, // no se puede copiar informacion
+      controller: _inputDateController,
+      onTap: () {
+        //Quita el focus
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+      //decoration
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        labelText: 'Fecha de nacimiento',
+        hintText: 'DD/MM/YYYY',
+        suffixIcon: Icon(Icons.calendar_today),
+        icon: Icon(Icons.perm_contact_calendar),
+      ),
+    );
+  }
+
+  Future _selectDate(BuildContext context) async {
+    //mostrar el valor
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(2018),
+      lastDate: new DateTime(2025),
+    );
+    if (picked != null) {
+      setState(() {
+        _fecha = picked.toString();
+        _inputDateController.text = _fecha;
+      });
+    }
   }
 }
